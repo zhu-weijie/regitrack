120ac3040bef5531c08af37c604b5da075a3beaa
zhuwj <byzhubaiyuan@gmail.com>
2025-08-15 18:24:09 +0800

    Create the Vehicle Table Component

---
+++ apps/client/src/components/VehicleTable.tsx
@@ -0,0 +1,61 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Chip,
+  Box,
+  Typography,
+} from '@mui/material';
+import { Vehicle } from '../types/vehicle';
+
+interface VehicleTableProps {
+  vehicles: Vehicle[];
+}
+
+export const VehicleTable = ({ vehicles }: VehicleTableProps) => {
+  return (
+    <Box sx={{ padding: 3 }}>
+      <Typography variant="h4" gutterBottom>
+        Vehicle Registrations
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="vehicle table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Brand</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>Class</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>Registration Date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {vehicles.map((vehicle) => (
+              <TableRow key={vehicle.uuid}>
+                <TableCell>{vehicle.brand}</TableCell>
+                <TableCell>{vehicle.type}</TableCell>
+                <TableCell style={{ textTransform: 'capitalize' }}>
+                  {vehicle.vehicle_class}
+                </TableCell>
+                <TableCell>
+                  <Chip
+                    label={vehicle.status}
+                    color={
+                      vehicle.status === 'REGISTERED' ? 'success' : 'error'
+                    }
+                    size="small"
+                  />
+                </TableCell>
+                <TableCell>{vehicle.start_date}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
