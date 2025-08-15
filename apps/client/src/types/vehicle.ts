3a6b32735d2eabebb7f738c12ce27c1390631fa9
zhuwj <byzhubaiyuan@gmail.com>
2025-08-15 19:08:25 +0800

    Run linting

--- apps/client/src/types/vehicle.ts
+++ apps/client/src/types/vehicle.ts
@@ -1,11 +1,11 @@
 export interface Vehicle {
-    uuid: string;
-    status: 'REGISTERED' | 'DEREGISTERED';
-    start_date: string;
-    end_date: string;
-    time: string;
-    user_uuid: string;
-    brand: string;
-    type: '5-seat' | '7-seat';
-    vehicle_class: 'personal' | 'commercial';
+  uuid: string;
+  status: 'REGISTERED' | 'DEREGISTERED';
+  start_date: string;
+  end_date: string;
+  time: string;
+  user_uuid: string;
+  brand: string;
+  type: '5-seat' | '7-seat';
+  vehicle_class: 'personal' | 'commercial';
 }
