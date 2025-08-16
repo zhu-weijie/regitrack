6742019857511094cb0ca4ed34572cd07a9877c7
zhuwj <byzhubaiyuan@gmail.com>
2025-08-16 19:00:00 +0800

    Define the Provider

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" # You can change this to your preferred region
+}
